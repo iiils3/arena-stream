7ef6c85255f83b09d4484104ad6920e24b2829ef
Ali Ismail Hameed Mohammed <ali.23sc124@student.uomosul.edu.iq>
2026-09-19 01:44:41 +0300

    Track final life state

--- server/src/state.ts
+++ server/src/state.ts
@@ -25,6 +25,7 @@ export class PlayerState extends Schema {
   @type("number") maxHp = 5;
   @type("boolean") alive = true;
   @type("boolean") weakened = false;
+  @type("boolean") finalLife = false;
 }
 
 export class ArenaState extends Schema {
