50b59d493af574e759de4f462b1efaa58e3fccbf
Ali Ismail Hameed Mohammed <ali.23sc124@student.uomosul.edu.iq>
2026-09-19 16:45:07 +0300

    Harden melee hitbox detection

--- godot/scripts/combat/hitbox.gd
+++ godot/scripts/combat/hitbox.gd
@@ -1,12 +1,10 @@
 extends Area2D
 class_name ArenaHitbox
-
-var owner_id := -1
-var owner_team := -1
+var owner_id:=-1
+var owner_team:=-1
 var weapon_data:ArenaWeaponData
-var facing := 1
-var already_hit := {}
-
+var facing:=1
+var already_hit:={}
 func setup(p_owner_id:int,p_team:int,p_weapon:ArenaWeaponData,p_facing:int)->void:
     owner_id=p_owner_id
     owner_team=p_team
@@ -17,13 +15,12 @@ func setup(p_owner_id:int,p_team:int,p_weapon:ArenaWeaponData,p_facing:int)->voi
     monitorable=true
     collision_layer=4
     collision_mask=8
-
 func scan()->void:
-    if weapon_data==null: return
+    if weapon_data==null:return
     for area in get_overlapping_areas():
-        if not area is ArenaHurtbox: continue
+        if not area is ArenaHurtbox:continue
         var hurt:=area as ArenaHurtbox
-        if hurt.player_id==owner_id or already_hit.has(hurt.player_id): continue
-        if hurt.team==owner_team: continue
+        if hurt.player_id==owner_id or already_hit.has(hurt.player_id):continue
+        if hurt.team==owner_team:continue
         already_hit[hurt.player_id]=true
         hurt.receive_hit(owner_id,owner_team,weapon_data.hits_to_kill,weapon_data.knockback)
